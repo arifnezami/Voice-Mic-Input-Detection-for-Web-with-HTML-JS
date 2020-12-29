809b28a42f053dbbffb8b92b174e6aa015d4017f
Arif Nezami <arifnezami@gmail.com>
2020-12-29 13:35:49 +0600

    Update main.js

--- main.js
+++ main.js
@@ -97,7 +97,7 @@ function drawLoop( time ) {
     {   console.log(meter);
         canvasContext.fillStyle = "red";
         jump();
-       // e.preventDefault();
+       preventDefault();
     }
     else
         canvasContext.fillStyle = "green";
